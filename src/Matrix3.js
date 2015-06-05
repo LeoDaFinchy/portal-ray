847a5e358312f1a6966ec7f359b6480b85480883
Adam Finch <adam@migreat.com>
2015-06-05 17:03:46 +0100

    Can apply a matrix to another

--- src/Matrix3.js
+++ src/Matrix3.js
@@ -7,6 +7,23 @@ var Matrix3 = function()
     ];
 };
 
+Object.defineProperty(Matrix3.prototype, 'applyMatrix3', {
+    value: function(other){
+        this.m = [
+            (this.m[0] * other.m[0]) + (this.m[1] * other.m[3]) + (this.m[2] * other.m[6]),
+              (this.m[0] * other.m[1]) + (this.m[1] * other.m[4]) + (this.m[2] * other.m[7]),
+                (this.m[0] * other.m[2]) + (this.m[1] * other.m[5]) + (this.m[2] * other.m[8]),
+            (this.m[3] * other.m[0]) + (this.m[4] * other.m[3]) + (this.m[5] * other.m[6]),
+              (this.m[3] * other.m[1]) + (this.m[4] * other.m[4]) + (this.m[5] * other.m[7]),
+                (this.m[3] * other.m[2]) + (this.m[4] * other.m[5]) + (this.m[5] * other.m[8]),
+            (this.m[6] * other.m[0]) + (this.m[7] * other.m[3]) + (this.m[8] * other.m[6]),
+              (this.m[6] * other.m[1]) + (this.m[7] * other.m[4]) + (this.m[8] * other.m[7]),
+                (this.m[6] * other.m[2]) + (this.m[7] * other.m[5]) + (this.m[8] * other.m[8])
+        ];
+        return this;
+    }
+});
+
 Object.defineProperty(Matrix3, 'identity', {
     value: function(){
         return new Matrix3();
