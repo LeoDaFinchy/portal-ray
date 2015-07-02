2c832b8ab28f5c8bb59d177fb99058b0e3b27a19
Adam Finch <adam@migreat.com>
2015-07-02 18:24:25 +0100

    Created LineSegment2Collection

--- src/LineSegment2.js
+++ src/LineSegment2.js
@@ -4,7 +4,13 @@ var LineSegment2 = function(a, b)
     this.b = b;
 };
 
+var LineSegment2Collection = function(){};
+LineSegment2Collection.prototype = [];
+
 exports['LineSegment2'] = LineSegment2;
+exports['LineSegment2Collection'] = LineSegment2Collection;
+
+var Intersect2 = require('./Intersect2').Intersect2;
 
 Object.defineProperties(LineSegment2.prototype, {
     offset:{
@@ -28,6 +34,34 @@ Object.defineProperties(LineSegment2.prototype, {
             .rotate(Math.atan2(this.offset.y, this.offset.x))
             .scale(new Vector2(this.offset.length, 1))
             ;
+        }
+    }
+});
+
+Object.defineProperties(LineSegment2Collection.prototype, {
+    intersectSelf: {
+        value: function(){
+            var results = [];
+            for(var i = 0; i < this.length; i++)
+            {
+                for(var j = i + 1; j < this.length; j++)
+                {
+                    results.push(new Intersect2(this[i], this[j]));
+                }
+            }
+            return results;
+        }
+    },
+    intersect: {
+        value: function(other){
+            var results = [];
+            for(var i = 0; i < this.length; i++)
+            {
+                for(var j = 0; j < other.length; j++)
+                {
+                    results.push(new Intersect2(this[i], other[j]));
+                }
+            }
         }
     }
 });
