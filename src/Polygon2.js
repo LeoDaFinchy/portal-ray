8226a98f35d25704b80200d0bdf8c322a2bd5747
Adam <leodafinchy@gmail.com>
2015-07-15 18:41:07 +0100

    Can now generate regular polygons

--- src/Polygon2.js
+++ src/Polygon2.js
@@ -12,6 +12,7 @@ exports['Polygon2'] = Polygon2;
 exports['Polygon2Collection'] = Polygon2Collection;
 
 var LineSegment2 = require('./LineSegment2').LineSegment2;
+var Vector2 = require('./Vector2').Vector2;
 
 Object.defineProperties(Polygon2.prototype, {
     edges: {
@@ -20,6 +21,24 @@ Object.defineProperties(Polygon2.prototype, {
                 var otherIndex = (index + 1) % array.length;
                 return new LineSegment2(value, array[otherIndex]);
             });
+        }
+    }
+});
+
+Object.defineProperties(Polygon2, {
+    regular: {
+        value: function(corners, radius, turns){
+            turns = turns || 1;
+            var angle = Math.PI * 2.0 * turns / corners;
+            return new Polygon2(_
+                .range(0, corners)
+                .map(function(x){
+                    return x * angle;
+                })
+                .map(function(x){
+                    return new Vector2(Math.cos(x) * radius, Math.sin(x) * radius);
+                })
+            );
         }
     }
 });
