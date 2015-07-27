57736c63bdf86d8c902734a293b70c370fd0dd85
Adam <leodafinchy@gmail.com>
2015-07-27 19:26:04 +0100

    Added down listeners

--- src/Hitzone2.js
+++ src/Hitzone2.js
@@ -2,6 +2,7 @@ var _ = require('underscore')._;
 
 var Hitzone2 = function(visualiser){
     this.visualiser = visualiser || new Visualiser2();
+    this.downListeners = [];
 };
 
 exports['Hitzone2'] = Hitzone2;
@@ -15,7 +16,19 @@ Object.defineProperties(Hitzone2.prototype,{
                 .subtract(new Vector2(event.target.offsetLeft, event.target.offsetTop));
             this.visualiser.draw(null, context);
             var hit = context.isPointInPath(coords.x, coords.y);
-            console.info(this, hit);
+
+            if(hit){
+                if(event.type === "mousedown" || event.type === "touchstart")
+                {
+                    _.each(this.downListeners, function(x){x(event);});
+                }
+            }
+        }
+    },
+    onDown: {
+        value: function(listener)
+        {
+            this.downListeners.push(listener);
         }
     }
 });
