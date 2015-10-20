f6114bede781348e413e19a781ecd41e0131ddf4
Adam Finch <adam@migreat.com>
2015-10-20 13:13:05 +0100

    Made Chaos not crap

--- src/engine/Chaos.js
+++ src/engine/Chaos.js
@@ -1,6 +1,6 @@
 function Chaos(input, then){
     window.crypto.subtle.digest("sha-256", Chaos.encoder.encode(input)).then(function(x){
-        then(Chaos.encoder.encode(Chaos.decoder.decode(x)));
+        then(Chaos.encoder.encode(Chaos.toHex(x)));
     })
 }
 
@@ -8,9 +8,20 @@ exports.Chaos = Chaos;
 
 Object.defineProperties(Chaos, {
     encoder:{
-        value: new window.TextEncoder()
+        value: new window.TextEncoder('utf-8')
     },
-    decoder:{
-        value: new window.TextDecoder()
+    toHex: {
+        value: function(input){
+            var codes = [];
+            var data = new DataView(input);
+            var zeros = '00000000'
+            for (var i = 0; i < data.byteLength; i += 4) {
+                var num = data.getUint32(i)
+                var chars = num.toString(32)
+                var paddedValue = (zeros + chars).slice(-zeros.length)
+                codes.push(paddedValue);
+            }
+            return codes.join("");
+        }
     }
 });
